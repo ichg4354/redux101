86fe95a0160cc07e77ae34f5abfda5a4ef7f7158
user <60804817+ichg4354@users.noreply.github.com>
2020-10-08 21:35:14 +0900

    useSlice

--- src/Store.js
+++ src/Store.js
@@ -9,22 +9,6 @@ import {
 export const addToDo = createAction("ADD");
 export const removeToDo = createAction("REMOVE");
 
-// const reducer = (state = [], action) => {
-//   if (action.type === addToDo.type) {
-//     return [{ text: action.payload, id: Date.now() }, ...state];
-//   } else if (action.type === removeToDo.type) {
-//     return state.filter((each) => each.id !== action.payload);
-//   }
-// };
-
-// const reducer = createReducer([], {
-//   [addToDo]: (state, action) => {
-//     state.push({ text: action.payload, id: Date.now() });
-//   },
-//   [removeToDo]: (state, action) =>
-//     state.filter((each) => each.id !== action.payload),
-// });
-
 export const toDos = createSlice({
   name: "toDosReducer",
   initialState: [],
@@ -42,3 +26,18 @@ console.log(toDos);
 const store = configureStore({ reducer: toDos.reducer });
 
 export default store;
+// const reducer = (state = [], action) => {
+//   if (action.type === addToDo.type) {
+//     return [{ text: action.payload, id: Date.now() }, ...state];
+//   } else if (action.type === removeToDo.type) {
+//     return state.filter((each) => each.id !== action.payload);
+//   }
+// };
+
+// const reducer = createReducer([], {
+//   [addToDo]: (state, action) => {
+//     state.push({ text: action.payload, id: Date.now() });
+//   },
+//   [removeToDo]: (state, action) =>
+//     state.filter((each) => each.id !== action.payload),
+// });
